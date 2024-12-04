12e166a6c45a9bd5275088b7737e58da0ba9c1e4
DESKTOP-M9EUJ5F\Raiden <h.r.sumabat@gmail.com>
2024-12-04 19:50:58 +0800

    adding to rout

--- TranRoutes/Services/NumberOfTripsByDistance.cs
+++ TranRoutes/Services/NumberOfTripsByDistance.cs
@@ -18,7 +18,7 @@ namespace TranRoutes.Services
         int _distance = 0;
 
         HashSet<(string Path, int Distance)> _result = new();
-        HashSet<(string Path, int Distance)> _routing = new();
+        HashSet<(string Path, int Distance, int Stops)> _routing = new();
         HashSet<(string Value, int Distance)> _visited = new();
         public NumberOfTripsByDistance(IRouteRepository repo)
         {
@@ -31,22 +31,6 @@ namespace TranRoutes.Services
             _distance = distance;
             _records = _repo.Get();
 
-            //var r = _records.SingleOrDefault(w => w.Source == source);
-            //if (r == null) return 0;
-            //foreach (var n in r.NearList)
-            //{
-            //    toDestination.Clear();
-            //    toDestination.Add((_source, 0));
-            //    toDestination.Add((n.Destination, n.Distance));
-            //    int sumDistance = toDestination.Sum(s => s.Distance);
-            //    string p = String.Join("=>", toDestination.Select(s => s.Path));
-            //    var newPath = (p, sumDistance);
-            //    _routing.Add(newPath);
-            //    Analayze(n.Destination);
-            //}
-
-            //toDestination.Add((_source, 0));
-            //Analayze(source);
             var result = new List<(string Path, int Distance)>();
             Find(source, 0, new List<(string Path, int Distance)>(), result);
             return 0;
@@ -58,7 +42,13 @@ namespace TranRoutes.Services
             var current = (source, distance);
 
             if (_visited.Contains(current))
+            {
+                string path = String.Join("=>", currentRoute.Select(s => s.Path));
+                int totalDistance = currentRoute.Sum(s => s.Distance);
+                int stops = currentRoute.Count() - 1;
+                _routing.Add((path, totalDistance, stops));
                 return;
+            }
 
             _visited.Add(current);
             currentRoute.Add(current);
@@ -98,44 +88,44 @@ namespace TranRoutes.Services
         //        Find(near.Destination, near.Distance, r);
         //}
 
-        void Analayze(string current)
-        {
-            var record = _records.SingleOrDefault(w => w.Source == current);
-            if (record == null) return;
+        //void Analayze(string current)
+        //{
+        //    var record = _records.SingleOrDefault(w => w.Source == current);
+        //    if (record == null) return;
 
-            foreach (var near in record.NearList)
-            {
-                toDestination.Add((near.Destination, near.Distance));
-                int sumDistance = toDestination.Sum(s => s.Distance);
-                string p = String.Join("=>", toDestination.Select(s => s.Path));
-                var newPath = (p, sumDistance);
-                if (sumDistance > _distance)
-                {
-                    toDestination.Clear();
-                    toDestination.Add((_source, 0));
-                    continue;
-                }
-                if (_routing.Contains(newPath))
-                {
-                    return;
-                }
+        //    foreach (var near in record.NearList)
+        //    {
+        //        toDestination.Add((near.Destination, near.Distance));
+        //        int sumDistance = toDestination.Sum(s => s.Distance);
+        //        string p = String.Join("=>", toDestination.Select(s => s.Path));
+        //        var newPath = (p, sumDistance);
+        //        if (sumDistance > _distance)
+        //        {
+        //            toDestination.Clear();
+        //            toDestination.Add((_source, 0));
+        //            continue;
+        //        }
+        //        if (_routing.Contains(newPath))
+        //        {
+        //            return;
+        //        }
 
-                _routing.Add(newPath);
-                Analayze(near.Destination);
+        //        _routing.Add(newPath);
+        //        Analayze(near.Destination);
 
-                //if ((near.Destination == _destination) && (sumDistance <= _distance))
-                //{
-                //    if (_result.Contains(newPath)) return;
-                //    _result.Add(newPath);
-                //}
-                //if (sumDistance > _distance)
-                //{
-                //    toDestination.Clear();
-                //    toDestination.Add((_source, 0));
-                //    return;
-                //}
-                //Analayze(near.Destination);
-            }
-        }
+        //        //if ((near.Destination == _destination) && (sumDistance <= _distance))
+        //        //{
+        //        //    if (_result.Contains(newPath)) return;
+        //        //    _result.Add(newPath);
+        //        //}
+        //        //if (sumDistance > _distance)
+        //        //{
+        //        //    toDestination.Clear();
+        //        //    toDestination.Add((_source, 0));
+        //        //    return;
+        //        //}
+        //        //Analayze(near.Destination);
+        //    }
+        //}
     }
 }
